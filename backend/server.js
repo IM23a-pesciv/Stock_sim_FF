fdf9e46d28d7edbf3a873baa136e4d23d7819813
oldOmen <victor.pesci@outlook.com>
2026-06-09 11:03:21 +0200

    fixed?

--- backend/server.js
+++ backend/server.js
@@ -364,7 +364,7 @@ io.on('connection', (socket) => {
     });
 });
 
-const PORT = process.env.PORT || 3000;
+const PORT = process.env.PORT || 5000;
 
 server.listen(PORT, () => {
     console.log(`Server running at http://localhost:${PORT}`);
